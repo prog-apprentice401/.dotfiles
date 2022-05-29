87c5dd28b2f172072ea05309a58133f5e5f45800
prog-apprentice401 <koustubhsrivastava1@gmail.com>
2022-05-29 16:29:12 +0530

    made fish vi mode a tad more beautiful

---
+++ .config/fish/functions/fish_default_mode_prompt.fish
@@ -0,0 +1,25 @@
+function fish_default_mode_prompt --description 'Display vi prompt mode'
+    # Do nothing if not in vi mode
+    if test "$fish_key_bindings" = fish_vi_key_bindings
+        or test "$fish_key_bindings" = fish_hybrid_key_bindings
+        switch $fish_bind_mode
+            case default
+                set_color --bold red
+                echo ' '
+            case insert
+                set_color --bold green
+                echo ' '
+            case replace_one
+                set_color --bold green
+                echo ' '
+            case replace
+                set_color --bold cyan
+                echo ' '
+            case visual
+                set_color --bold magenta
+                echo ' '
+        end
+        set_color normal
+        echo -n ' '
+    end
+end
