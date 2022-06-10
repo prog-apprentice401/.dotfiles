6829c6aaa5ab5c973bbddaade53736b144fdc897
prog-apprentice401 <koustubhsrivastava1@gmail.com>
2022-06-10 20:45:02 +0530

    made desktop more pointy, (this time for real)

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -13,7 +13,7 @@ echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 if type "xrandr"; then
 	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
 		MONITOR=$m polybar --reload bar1 &
-		MONITOR=$m polybar --reload dummy &
+		#MONITOR=$m polybar --reload dummy &
 	done
 else
 	polybar --reload example &
